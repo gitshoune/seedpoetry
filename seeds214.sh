db73f542b6ffa2374ff88cfb4695d831b689be82
gitshoune <98457299+gitshoune@users.noreply.github.com>
2025-09-29 13:10:03 +0800

    Update seeds214.sh

--- seeds214.sh
+++ seeds214.sh
@@ -1,17 +1,17 @@
-START_DATE="2025-02-01"
+START_DATE="2025-04-01"
 DAYS=90
 FILE="README.md"
 
 # Calculate start and end dates in seconds since epoch
 START_SECONDS=$(date -d "$START_DATE" "+%s")
-END_SECONDS=$(date "+%s")  # Current date/time
+END_SECONDS=$(date "+%s")  # Current date/time (2025-09-29)
 
 # Calculate the total number of seconds in the date range
 RANGE_SECONDS=$((END_SECONDS - START_SECONDS))
 
 for ((i=0; i<$DAYS; i++)); do
   # Generate a random number of seconds within the date range
-  RANDOM_SECONDS=$((RANDOM % RANGE_SECONDS))
+  RANDOM_SECONDS=$(shuf -i 0-$RANGE_SECONDS -n 1)
   # Calculate random commit date
   COMMIT_SECONDS=$((START_SECONDS + RANDOM_SECONDS))
   COMMIT_DATE=$(date -d "@$COMMIT_SECONDS" "+%Y-%m-%dT%H:%M:%S")
